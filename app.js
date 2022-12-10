bb36b6be1f911ae58f45d34b9e2a4a8680db7857
sphejiahuan <iamtony_zhou@yahoo.com>
2022-12-10 14:36:00 -0600

    main

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 app.set('view engine', 'ejs');
 
@@ -136,6 +138,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
